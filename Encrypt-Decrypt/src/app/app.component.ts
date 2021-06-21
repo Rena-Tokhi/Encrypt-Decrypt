0f9c48899aa74023080e8020efb947e261d07e5f
Rina Tokhi <Rina.Tokhi7@gmail.com>
2021-06-21 14:39:30 +0430

    finshed

--- Encrypt-Decrypt/src/app/app.component.ts
+++ Encrypt-Decrypt/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  
+import * as CryptoJS from 'crypto-js';  
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Encrypt-Decrypt';
+@Component({  
+  selector: 'app-root',  
+  templateUrl: './app.component.html',  
+  styleUrls: ['./app.component.css']  
+})  
+export class AppComponent {  
+  title = 'EncryptionDecryptionSample';  
+
+  plainText:any;  
+  encryptText: any;  
+  encPassword: any;  
+  decPassword:any;  
+  conversionEncryptOutput: any;  
+  conversionDecryptOutput:any;  
+
+  constructor() {  
+  }  
+  //method is used to encrypt and decrypt the text  
+  convertText(conversion:any) {  
+      if (conversion=="encrypt") {  
+        this.conversionEncryptOutput = CryptoJS.AES.encrypt(this.plainText.trim(), this.encPassword.trim()).toString();  
+      }  
+      else {  
+        this.conversionDecryptOutput = CryptoJS.AES.decrypt(this.encryptText.trim(), this.decPassword.trim()).toString(CryptoJS.enc.Utf8);  
+
+    }  
+  }  
 }
